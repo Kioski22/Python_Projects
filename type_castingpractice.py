00aa211147f81f2bee72dffed4cf67819d30d27a
Kioski22 <floresrandy.m@gmail.com>
2025-06-09 13:52:03 +0800

    fix ko later

---
+++ type_castingpractice.py
@@ -0,0 +1,13 @@
+# this is a personal budget tracker
+
+name = input("input your name: ")
+data1 =  
+data2 = data1.split(", ")
+
+print(data2)
+
+
+# data = ["food:100", "transport:50"]
+# for item in data:
+#     parts = item.split(":")
+#     print("Category:", parts[0], "Amount:", parts[1])
