cfa4275b374a1050deb18c3ed1bf3b749f97c2ed
kioski21 <152053430+kioski21@users.noreply.github.com>
2025-06-15 09:55:33 +0800

    Adds expense tracking functionality to the budget tracker
    Implements a new feature for users to input and store expenses or income.
    
    Users can enter expenses in a specified format and receive feedback on incorrect inputs.
    
    The total amount of recorded expenses is calculated and displayed at the end.

--- type_castingpractice.py
+++ type_castingpractice.py
@@ -1,13 +1,38 @@
 # this is a personal budget tracker
+name = input('enter your name : ')
+expense = []
 
-name = input("input your name: ")
-data1 =  
-data2 = data1.split(", ")
+def storeExpense (name, expense):
+    print(f"\n Hi! {name} please start entering your expenses or income thank you <3")
+    print ("use this format to enter your expenses (amount, description)")
+    print("type 'done' when you are finished ")
+    while True:
+       addExpense = input('please enter your expense or income: ')
+       if addExpense.lower() == "done":
+           break
+       try:
+           amountString , description = addExpense.split(", ")
+           amountString = float(amountString)
+           expense.append({"amount": amountString , "description": description})
+       except ValueError:
+           print("oh no  you've entered a wrong value :<")
+storeExpense(name, expense)
+print("\nYour Recorded Expense/Income")
+for item in expense:
+    print(f"{item['description']}:  ₱{item['amount']}")
 
-print(data2)
+total = 0
+for item in expense:
+    total += item['amount']
 
+print(f"\nTotal: ₱{total}")
 
-# data = ["food:100", "transport:50"]
-# for item in data:
-#     parts = item.split(":")
-#     print("Category:", parts[0], "Amount:", parts[1])
+           
+           
+           
+       
+ 
+            
+               
+           
+       
