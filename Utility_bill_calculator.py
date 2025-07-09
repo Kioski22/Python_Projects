33e480494badde07648fb8edf0c24e3b9e5401ca
kioski21 <152053430+kioski21@users.noreply.github.com>
2025-07-10 00:00:31 +0800

    update pa

--- Utility_bill_calculator.py
+++ Utility_bill_calculator.py
@@ -2,15 +2,37 @@ print("welcome to your personal Utility Bill Calculator!")
 
 name = input("may i know what your name would be ?: \n")
 billType = input("what type of bill do you want to compute? (kwh, cm, gb): \n")
-billType = billType.lower(billType)
+billType = billType.lower()
 billAmount = int(input("input the bill amount"))
 
-if billType == "kwh" and billAmount <= 100:
+if billType == "kwh" and billAmount >= 100:
     print("you've entered and electricity bill")
-    billTotal = billAmount * 10 / 10
-    print ("your bill amout")
-elif billAmount
-    billTotal = billAmount * 20 /20
+    billTotal = billAmount * 1 + 3
+    print("Hi! {name} your bill amount" + " " + str(billTotal))
+    try:
+        billTotal = billAmount + 8 
+        print("Hi! {name} your bill amount" + " " + str(billTotal))
+    except:
+        print("there's something wrong")
+
+
+elif billType == "cm" and billAmount !=0 :
+    billTotal = billAmount * 20 
+    print("Hi! {name} your bill amount" + " " + str(billTotal))
+    try:
+        billTotal = billAmount + 8 
+    except ValueError:
+        print("there's something wrong")
+
+elif billType == "gb" and billAmount != 0:
+    billTotal = billAmount * 20 
+    print("Hi! {name} your bill amount" + " " + str(billTotal))
+    try:
+        billTotal = billAmount + 8 
+    except ValueError:
+        print("there's something wrong")
+else :
+    print ("check your code :>")
 
     
 
